2c343f80ef555cb0eeee7a31a6edd8d4cf3db8e2
Ga A <60027994+pikolly@users.noreply.github.com>
2020-10-02 15:51:15 +0900

    report problem

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import axios from "axios";
 import Main from "./Main";
 import Info from "./Info";
 import Search from "./Search";
 import Footer from "./Footer";
 
 export default function App() {
-  return (
+  const [weather, setWeather] = useState({ ready: false });
+    console.log("hello");
+
+  function showResponse(response) {
+    console.log(response.data);
+    setWeather({
+      ready: true,
+      city:response.data.name,
+      temp: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.main.wind,
+      discription: response.data.weather[0].humidity,
+    });
+  }
+  if (weather.ready) {return (
     <div>
       <div className="App">
         <div className="container">
           <Search />
           <div className="row">
             <div className="col">
-              <Main city="Tokyo" temp={24} />
+              <Main defaultCity="Tokyo" temp={weather.temp} />
             </div>
             <div className="col">
               <Info
@@ -27,8 +42,14 @@ export default function App() {
           </div>
         </div>
       </div>
-
       <Footer />
     </div>
   );
+}
+else {
+  const apiKey = `651b42268fb8baaefb7c30359a32f85c`;
+  let unit = `metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${unit}`;
+  axios.get(url).then(showResponse);
+return (<div>Loading...</div>)
 }
